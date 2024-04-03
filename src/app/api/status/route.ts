cfd1749b7ff6822dc8ee91c4c464159be6cc2b38
Robert H Stolz <7143905+garden-of-delete@users.noreply.github.com>
2024-04-03 15:10:41 -0700

    use next.js app router instead of pages router

---
+++ src/app/api/status/route.ts
@@ -0,0 +1,6 @@
+import { fetchJson } from '@/lib/utils';
+import {NextResponse} from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json({}, { status: 200 });
+}
