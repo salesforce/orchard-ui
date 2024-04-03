4d6b5779980d0cc2e734160cc34bbe12acf7d869
Robert H Stolz <7143905+garden-of-delete@users.noreply.github.com>
2024-04-03 11:45:14 -0700

    add __status endpoint for probes

---
+++ src/pages/api/__status.tsx
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+    status: string;
+}
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.method === 'GET') {
+        res.status(200).json({ status: 'OK' });
+    } else {
+        // Method Not Allowed
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
