de6d1a1e831f642f9f6a44a658d5e8c5e9f403d9
Robert H Stolz <7143905+garden-of-delete@users.noreply.github.com>
2024-04-12 15:22:53 -0700

    fix npm build by forcing dynamic (#5)

--- src/app/api/stats/counts/route.tsx
+++ src/app/api/stats/counts/route.tsx
@@ -1,5 +1,7 @@
 import { fetchJson } from "@/lib/utils"
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const url = process.env.ORCHARD_HOST + '/v1/stats/counts'
   return fetchJson(url)
